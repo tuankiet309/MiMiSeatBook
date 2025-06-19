4dd9369b031ef5be397ef9344f070903b2691970
NGUYEN TUAN KIET Assistant/(Part) Data Service and Business <nguyentuankiet309203@lgcns.com>
2025-06-18 19:07:51 -0700

    Init

---
+++ SeatBook/src/main/java/com/mimi/SeatBook/SeatBookApplication.java
@@ -0,0 +1,13 @@
+package com.mimi.SeatBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatBookApplication.class, args);
+	}
+
+}
---
+++ SeatBook/src/test/java/com/mimi/SeatBook/SeatBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimi.SeatBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
